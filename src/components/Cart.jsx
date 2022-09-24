8739b1dbb2ffd5b1dff9604871cfc895a800e22e
gabriel yucra <gabriel.yucra_99@hotmail.com>
2022-09-24 17:28:16 -0300

    cartContext p2

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,15 +5,20 @@ const Cart = () => {
 
     const ctx = useContext(CartContext);
 
+    const {removeItem} = useContext(CartContext)
     return (
         <div>
             <h2>I'm a cart</h2>
             <button onClick={ctx.clear}>Borrar todo</button>
             {
-                ctx.cartList.map(item=> <li>{item.title}</li>
-                
-                
-                
+                ctx.cartList.map(item=> 
+                    <div>
+                        <h2>{item.title}</h2> <h1>${item.price}</h1>
+                        <img src={item.pictureUrl} alt="item"></img>
+                        <h2>Cantidad seleccionada: {item.cantidad}</h2>
+                        <h3>{item.description}</h3>
+                        <button onClick={()=>removeItem(item.id)} className='btn btn-danger'>Eliminar producto</button>
+                    </div>
                 )
             }
         </div>
--- src/components/CartContext.jsx
+++ src/components/CartContext.jsx
@@ -7,21 +7,21 @@ const CartContextProvider = ({children}) => {
     const [cartList, setCartList] = useState([]);
 
 
-    const addItem =(product, cantidad) =>
+    const addItem =(item, cantidad) =>
         {
-            setCartList([...cartList, product])
+            if(isInCart(item.id)) {
+                setCartList(cartList.map(product => {
+                    return product.id === item.id ? { ...product, cantidad: product.cantidad + cantidad } : product
+                }));
+            } else {
+                setCartList([...cartList, {...item, cantidad}]);
+            }
         }
 
-    const removeItem =(id)=>{
-        setCartList(cartList.filter(product => product.id !== id))
-    }
+    const removeItem =(id)=> setCartList(cartList.filter(product => product.id !== id))
 
     const clear =()=> setCartList([])
-    
-
-    const isInCart = (id)=>{
-        cartList.find(product => product.id === parseInt(id))
-    }
+    const isInCart = (id) => cartList.find(product => product.id === parseInt(id)) ? true : false;
 
     return (
         <div>
