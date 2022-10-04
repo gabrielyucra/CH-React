f8deb43b44fed3cddb0e49ee3cf2958648a2f743
gabriel yucra <gabriel.yucra_99@hotmail.com>
2022-10-04 19:34:24 -0300

    icono carrito con cantidad de items

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,9 +1,24 @@
+import { useContext } from "react";
+import { CartContext } from "./CartContext";
 const CartWidget = ()=>{
-    return(
-        <>
-            <img src="https://img.icons8.com/ios-filled/50/000000/shopping-cart.png" alt="logo de carrito" />
-        </>
+
+    const { totalProducts, cartList } = useContext(CartContext);
+
+    while(cartList.length === 0) {
+        return(
+            <>
+                <img src="https://img.icons8.com/ios-filled/50/000000/shopping-cart.png" alt="logo de carrito" />
+            </>
+        );
+    }
+
+    return (
+        <div>
+            <img src="https://img.icons8.com/ios-filled/50/000000/shopping-cart.png"/>
+            <span className="cant-item-cart-w">{totalProducts()}</span>
+        </div>
     );
+
 }
 
 export default CartWidget;
